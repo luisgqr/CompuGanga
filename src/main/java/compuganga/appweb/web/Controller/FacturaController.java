3d20d8cdf2fd8a0303463c66975ffb5e980f9dc2
Luis <luis_qr_05@hotmail.com>
2022-07-02 00:33:24 -0500

    Factura agregada

---
+++ src/main/java/compuganga/appweb/web/Controller/FacturaController.java
@@ -0,0 +1,15 @@
+package compuganga.appweb.web.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+@Controller
+public class FacturaController {
+    private static final String FACTURA_INDEX="factura/index";
+
+    @GetMapping("/factura/index")
+    public String index(Model model){
+        return FACTURA_INDEX;
+    }
+}
